7b258f41fe124bff232ac58ad8ee004a50ae3fb5
oceanp8 <148895610+oceanp8@users.noreply.github.com>
2024-10-07 16:14:25 +0000

    first commit

---
+++ Main.java
@@ -0,0 +1,19 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String info;
+        Scoreboard game = new Scoreboard("Red", "Blue");
+        // info = game.getScore();
+        // System.out.println(info);
+        // game.recordPlay(1);
+        // info = game.getScore();
+        // System.out.println(info);
+        game.recordPlay(0);
+        info = game.getScore();
+        System.out.println(info);
+
+    }
+    
+    
+}
