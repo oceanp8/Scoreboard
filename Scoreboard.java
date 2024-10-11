e7a397a9149d48f4ba550b76d22bae6d13712712
oceanp8 <148895610+oceanp8@users.noreply.github.com>
2024-10-11 16:31:50 +0000

    finished

--- Scoreboard.java
+++ Scoreboard.java
@@ -1,30 +1,32 @@
 
-public class Scoreboard
-{
+public class Scoreboard {
     private String teamOne;
     private String teamTwo;
     private int teamOneScore;
     private int teamTwoScore;
     private String activeTeam;
-    
 
-    public Scoreboard(String one, String two)
-    {
+    public Scoreboard(String one, String two) {
         teamOne = one;
         teamTwo = two;
         activeTeam = teamOne;
     }
-    public String getScore()
-    {
+
+    public String getScore() {
         return teamOneScore + "-" + teamTwoScore + "-" + activeTeam;
     }
-    public void recordPlay(int score)
-    {
-        if(score == 0);
-            if(teamOne.equals(activeTeam));
-            teamOneScore += score;
 
+    public void recordPlay(int score) {
 
+        if (score == 0) 
+        {
+            if (activeTeam.equals(teamOne)) activeTeam = teamTwo;
+            else if (activeTeam.equals(teamTwo)) activeTeam = teamOne;    
+        }
+        else 
+        {
+            if (teamOne.equals(activeTeam)) teamOneScore += score;
+            if (teamTwo.equals(activeTeam)) teamTwoScore += score;
+        }
     }
 }
-
